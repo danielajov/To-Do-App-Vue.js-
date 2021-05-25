9ecff0987c2a4ecfe6adaf5e1a91b2f969c83c76
Daniela Jovevska <danielajovevska@hotmail.com>
2021-05-25 22:27:19 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,68 @@
+<template>
+  <div id="app" class="container">
+    <img src="./assets/logo.png" class="logo" />
+    <todo-list></todo-list>
+  </div>
+</template>
+
+<script>
+import TodoList from "./components/TodoList.vue";
+
+export default {
+  components: { TodoList },
+  name: "app",
+  data() {
+    return {
+      components: {
+        TodoList
+      }
+    };
+  }
+};
+</script>
+
+<style lang="scss">
+* {
+  box-sizing: border-box;
+}
+
+.container {
+  max-width: 600px;
+  margin: 0 auto;
+}
+
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+.logo {
+  display: block;
+  margin: auto;
+  width: 50px;
+}
+
+// h1,
+// h2 {
+//   font-weight: normal;
+// }
+
+// ul {
+//   list-style-type: none;
+//   padding: 0;
+// }
+
+// li {
+//   display: inline-block;
+//   margin: 0 10px;
+//   height: 75px;
+// }
+
+// a {
+//   color: #42b983;
+// }
+</style>
